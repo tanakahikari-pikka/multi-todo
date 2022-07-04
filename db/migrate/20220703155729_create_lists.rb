ca48b1ae7875e649597e0a5b08bce9985bc66540
田中光 <hikari@tanakahikarinoMacBook-Pro.local>
2022-07-04 14:25:36 +0900

    Create database

---
+++ db/migrate/20220703155729_create_lists.rb
@@ -0,0 +1,10 @@
+class CreateLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lists do |t|
+      t.string :todo
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
