3a7e5b6cea64fd9c224fb857729693e9c42c7218
田中光 <hikari@tanakahikarinoMacBook-Pro.local>
2022-07-11 18:20:41 +0900

    validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :lists, dependent: :destroy
   accepts_nested_attributes_for :lists, allow_destroy: true
 
+  validates_presence_of :name
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   belongs_to :category
+  validates_presence_of :todo
 
   def self.multi_update(lists_params)
     lists_params.to_h.map do |id, list_param|
